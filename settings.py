0685d45954acfe8d2f9fd12e91633d156ad47953
Xhionity <wpleasegg@gmail.com>
2026-02-26 20:47:56 +0300

    Initial commit: Alien Invasion game

---
+++ settings.py
@@ -0,0 +1,19 @@
+class Settings:
+    '''Класс для хранения всех настроек игры "Инопланетное вторжение"'''
+
+    def __init__(self):
+        """Инициализируем настройки игры"""
+        # Параметры экрана
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+
+        # Настройки корабля
+        self.ship_speed = 2.5
+
+        # Параметры снаряда
+        self.bullet_speed = 2.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
